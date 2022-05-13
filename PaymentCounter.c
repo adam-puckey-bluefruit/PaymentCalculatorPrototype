439194f495d8a0bbb1d8c402ceb19451282ca6a4
Adam Puckey <101401994+adam-puckey-bluefruit@users.noreply.github.com>
2022-05-13 13:33:23 +0100

    Partial modifications based on initial peer review.

--- PaymentCounter.c
+++ PaymentCounter.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
-#include<time.h>
+
 int main()
 {
-    char *months[12] = {
+    enum  { success = 0, warning = 1, failure = -1 };
+    
+    const char *months[12] = {
         "January",
         "Feb",
         "March",
@@ -16,7 +18,7 @@ int main()
         "November",
         "December" };
     
-
+    
     double paymentAmount;
     double totalPaid = 0;
     int currentMonthIndex;
@@ -24,49 +26,44 @@ int main()
     double amountOwed;
 
     printf("Input amount owed here: ");
-    scanf("%lf", &amountOwed);
+    scanf("%lf", &amountOwed); //what if they hit return with no input? - CRASH do while loop? 
 
     printf("And how much will you be paying back each month? ");
     scanf("%lf", &paymentAmount);
     
     printf("Type in a number for the month will you start paying: 1.Jan 2.Feb 3.Mar 4.Apr 5.May 6.Jun 7.Jul 8.Aug 9.Sep 10.Oct 11.Nov 12.Dec ");
 
-    scanf(" %i", &currentMonthIndex);
+    scanf(" %i", &currentMonthIndex); // validation checking, error handling and enabling typing? range checking - what happens if 13 or 0 is typed? - CRASH! No input? CRASH!
     currentMonthIndex--;
 
     printf("and what year? ");
     scanf("%i", &currentYear);
     
-    while(amountOwed > paymentAmount) { //replace 0 with paymentAmount variable to fix
-        if(amountOwed < paymentAmount) {
-        amountOwed -= amountOwed;
+    while(amountOwed > 0) 
+    { 
+        if(amountOwed < paymentAmount) 
+        {
+            paymentAmount = amountOwed;
         }
     
         amountOwed -= paymentAmount;
         totalPaid += paymentAmount;
 
-        printf(months[currentMonthIndex]);
-        printf(" %i ", currentYear);
+        printf("%s, %d\n", months[currentMonthIndex], currentYear);
+        printf(" Total Paid: %.2f. Amount remaining: %.2f.\n ", totalPaid, amountOwed);
+        
         currentMonthIndex++;
-        if(currentMonthIndex == 12) {
+
+        if(currentMonthIndex > 11) 
+        {
             currentMonthIndex = 0;
-            }
+        }
         
-        
-        
-        if(currentMonthIndex == 11) {
+        if(currentMonthIndex == 0) 
+        {
             currentYear++;
         }
-         
-        
-        printf(" Total Paid: %lf. Amount remaining: %lf.\n ", totalPaid, amountOwed);
-    
     }
-        
-    
-return 0;
-
-    
-    
+    return 0;  
 }
 
